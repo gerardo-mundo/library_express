80e201f35d2e50f9bf6ca56dd54148f89dadda2f
Gerardo Mundo <ger.mundo66@gmail.com>
2025-03-13 17:57:43 -0600

    Delete files

--- src/routes/aurh.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-import { userValidationFields } from 'schemas/users/userValidationFields.schema';
-
-const router = Router();
-
-router.post('/register', userValidationFields);
