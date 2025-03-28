2a364ccae888bd1074d10fc1bc095860ec936c2f
Gerardo Mundo <ger.mundo66@gmail.com>
2025-03-27 19:32:21 -0600

    (add): UserController

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,38 @@
+import { ApiResponseHandler } from '@api/apiResponseHandler';
+import { IUser } from '@interfaces/user.interface';
+import { UserService } from '@services/user.service';
+import { Request, Response } from 'express';
+
+export class UserController {
+  private apiResponseHandler = new ApiResponseHandler();
+
+  constructor(private userService: UserService) {}
+
+  public async updateUserRole(req: Request, res: Response) {
+    try {
+      const userData: Pick<IUser, 'id' | 'role'> = req.body;
+
+      const result = await this.userService.updateUserRole(
+        userData.id,
+        userData.role
+      );
+
+      if (result.success) {
+        this.apiResponseHandler.successResponse(
+          res,
+          `El rol de usuario ha sido actualizado a: ${userData.role}`,
+          result.data
+        );
+      } else {
+        console.log(result.error);
+
+        this.apiResponseHandler.badRequestResponse(
+          res,
+          `Uno o más datos no son correctos. ${result.error}`
+        );
+      }
+    } catch (error) {
+      this.apiResponseHandler.internalServerErrorResponse(res, `${error}`);
+    }
+  }
+}
