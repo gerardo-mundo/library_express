3ad35d2926f12a324f33a64cda5380d43c6069e1
Gerardo Mundo <ger.mundo66@gmail.com>
2025-03-28 17:23:46 -0600

    (update) interfaces for IUserRepository and IUserSerice, create new type UserWithoutPassword

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,4 +1,5 @@
 import { User } from '@prisma/client';
+import { Result } from './api.interface';
 
 export interface IUser extends User {
   role: UserRoles;
@@ -8,6 +9,8 @@ export type InitialUserCreation = Omit<
   IUser,
   'id' | 'role' | 'last_session' | 'created_at'
 >;
+
+export type UserWithoutPassword = Omit<IUser, 'password'>;
 
 export interface UserCredentials {
   email: string;
@@ -22,6 +25,7 @@ export type UserRoles =
   | 'PROFESSOR';
 
 export interface IUserRepository {
+  FindAll(): Promise<UserWithoutPassword[]>;
   FindById(id: string): Promise<IUser | null>;
   Create(userData: Omit<IUser, 'id'>): Promise<IUser>;
   UpdateRole(id: string, role: UserRoles): Promise<IUser>;
@@ -29,8 +33,10 @@ export interface IUserRepository {
 }
 
 export interface IUserService {
-  getUsers(): Promise<IUser[]>;
+  getUsers(): Promise<Result<UserWithoutPassword[]>>;
   createUserAccount(
     userData: InitialUserCreation
   ): Promise<Omit<IUser, 'password' | 'last_session'>>;
+  login(userCredentials: UserCredentials): Promise<Result<string>>;
+  updateUserRole(userId: string, role: string): Promise<Result<IUser>>;
 }
