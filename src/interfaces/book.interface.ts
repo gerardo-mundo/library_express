fbdb0a81c73638b65d55f33cb4b72386f7f02fb7
Gerardo Mundo <ger.mundo66@gmail.com>
2025-05-06 22:00:12 -0600

    (add): new method for book updating

--- src/interfaces/book.interface.ts
+++ src/interfaces/book.interface.ts
@@ -10,10 +10,11 @@ export interface IBookRepository {
   Create(book: any): Promise<IBook>;
   FindById(bookId: number): Promise<IBook | null>;
   FindAll(): Promise<IBook[]>;
-  Update(book: BookCreationDTO): Promise<IBook>;
+  Update(bookId: number, book: BookCreationDTO): Promise<IBook>;
   Delete(bookId: number): Promise<IBook>;
 }
 
 export interface IBookService {
   createBook(book: BookCreationDTO): Promise<Result<IBook>>;
+  updateBookFields(book: BookCreationDTO): Promise<Result<IBook>>;
 }
