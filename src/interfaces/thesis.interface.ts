a104c0ab8ab6f5334624e35c23e38ae3e738ec83
Gerardo Mundo <ger.mundo66@gmail.com>
2025-08-07 20:31:38 -0600

    (add): thesis interface

--- src/interfaces/thesis.interface.ts
+++ src/interfaces/thesis.interface.ts
@@ -1,9 +1,21 @@
-export interface Thesis {
-  id?: number;
-  title: string;
-  author: string;
-  author_two?: string;
-  author_three?: string;
-  copies: number;
-  director: string;
+import { Thesis } from 'prisma/prisma-client';
+
+import { Result } from './api.interface';
+
+export interface IThesis extends Thesis {}
+
+export type ThesisCreationDTO = Omit<IThesis, 'id' | 'created_at'>;
+
+export interface IThesisRepository {
+  Create(Thesis: any): Promise<IThesis>;
+  FindById(ThesisId: number): Promise<IThesis | null>;
+  FindAll(): Promise<IThesis[]>;
+  Update(ThesisId: number, Thesis: ThesisCreationDTO): Promise<IThesis>;
+  Delete(ThesisId: number): Promise<IThesis>;
+}
+
+export interface IThesisService {
+  createThesis(Thesis: ThesisCreationDTO): Promise<Result<IThesis>>;
+  updateThesisFields(Thesis: ThesisCreationDTO): Promise<Result<IThesis>>;
+  deleteThesisbyId(ThesisId: number): Promise<Result<IThesis>>;
 }
